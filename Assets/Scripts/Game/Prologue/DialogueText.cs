4253a87d1e2eb8b61bc4b052505fd91d9250de18
Dennis <dennis@gamelevelone.com>
2017-07-20 12:06:45 +0700

    changed some file names, changed script, modified scenePrologue, added textbox animation in prologue

--- Assets/Scripts/Game/Prologue/DialogueText.cs
+++ Assets/Scripts/Game/Prologue/DialogueText.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrologueText : MonoBehaviour {
+public class DialogueText : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
